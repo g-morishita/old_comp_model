9f88bd92071a09c24cd9f1adddd00974f85d3433
gota morishita <optimistic.gota@gmail.com>
2022-02-14 22:51:02 +1100

    minor change

--- agents.py
+++ agents.py
@@ -27,8 +27,7 @@ class Q_learner_softmax(Agent):
             self,
             learning_rate: float,
             inverse_temperature: float,
-            initial_values: Sequence[Union[int, float]]
-            ) -> None:
+            initial_values: Sequence[Union[int, float]]) -> None:
         self.estimated_values = np.array(initial_values, dtype=float)
 
         if (0 < learning_rate) and (learning_rate < 1.0):
@@ -40,7 +39,7 @@ class Q_learner_softmax(Agent):
         self.inverse_temperature = inverse_temperature
 
     def choose_action(self) -> int:
-        action_probs = self.softmax()
+        action_probs = self._softmax()
         chosen_action = np.random.choice(
                 len(action_probs),
                 size=1,
@@ -51,7 +50,7 @@ class Q_learner_softmax(Agent):
     def learn(self, chosen_action: int, reward: float) -> None:
         self.estimated_values[chosen_action] = self.estimated_values[chosen_action] + self.learning_rate * (reward - self.estimated_values[chosen_action])
 
-    def softmax(self) -> Sequence[float]:
+    def _softmax(self) -> Sequence[float]:
         exponents = self.inverse_temperature * self.estimated_values
         max_exp = np.max(exponents)
 
