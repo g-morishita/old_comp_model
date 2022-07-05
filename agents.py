383cfc43929c5622019acf903b95f202c81e6167
Ubuntu <ubuntu@gota-hmm-estimation.novalocal>
2022-07-05 01:48:33 +0000

    implement fit

--- agents.py
+++ agents.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 from abc import ABC, abstractclassmethod
+from functools import partial
 from typing import Union, Sequence
+
+from scipy.optimize import minimize
 
 
 class Agent(ABC):
@@ -9,6 +12,7 @@ class Agent(ABC):
     Agent class is supposed to be in charge of
     choosing actions and updating hyperparameters.
     """
+
     def __init__(self):
         self.estimated_values = None
 
@@ -23,11 +27,7 @@ class Agent(ABC):
         pass
 
     @abstractclassmethod
-    def fit(
-            self,
-            observed_actions: Sequence[int],
-            observed_rewards: Sequence[int]
-            ):
+    def fit(self, observed_actions: Sequence[int], observed_rewards: Sequence[int]):
         """estimate free parameters with maximum likelihood esimation."""
         pass
 
@@ -36,11 +36,10 @@ class EpsilonGreedy(Agent):
     """
     EpsilonGreedy implements a agent that follows the epsilon greeddy method.
     """
+
     def __init__(
-            self,
-            epsilon: float,
-            initial_values: Sequence[Union[int, float]]
-            ) -> None:
+        self, epsilon: float, initial_values: Sequence[Union[int, float]]
+    ) -> None:
         self.epsilon = epsilon
         self.estimated_values = np.array(initial_values, dtype=float)
         self.n_chosen_actions = np.zeros(len(initial_values))
@@ -51,14 +50,13 @@ class EpsilonGreedy(Agent):
         max_index = self.estimated_values[self.choose_action == max_value]
         max_index = np.random.choice(max_index)
 
-        choice_prob = np.tile(self.epsilon / (self.estimated_values.shape[0] - 1), self.estimated_values.shape[0])
+        choice_prob = np.tile(
+            self.epsilon / (self.estimated_values.shape[0] - 1),
+            self.estimated_values.shape[0],
+        )
         choice_prob[max_index] = 1 - self.epsilon
 
-        chosen_action = np.random.choice(
-                len(choice_prob),
-                size=1,
-                p=choice_prob
-                )
+        chosen_action = np.random.choice(len(choice_prob), size=1, p=choice_prob)
 
         # Increment the number of chosen actions.
         self.n_chosen_actions[chosen_action] += 1
@@ -67,14 +65,15 @@ class EpsilonGreedy(Agent):
 
     def learn(self, chosen_action: int, reward: float) -> None:
         n_chosen_actions = self.n_chosen_actions[chosen_action]
-        self.estimated_values[chosen_action] = self.estimated_values[chosen_action] * (n_chosen_actions - 1) + reward
+        self.estimated_values[chosen_action] = (
+            self.estimated_values[chosen_action] * (n_chosen_actions - 1) + reward
+        )
         self.estimated_values /= n_chosen_actions
 
-    def fit(
-            self,
-            observed_actions,
-            observed_rewards
-            ) -> Sequence[float]:
+    def fit(self, observed_actions, observed_rewards) -> Sequence[float]:
+        pass
+
+    def calculate_ll(self, observed_actions, observed_rewards) -> Sequence[float]:
         pass
 
 
@@ -83,43 +82,94 @@ class QSoftmax(Agent):
     QSoftmax implements Q learning model
     and chooses an action using softmax function.
     """
+
     def __init__(
-            self,
-            learning_rate: float,
-            inverse_temperature: float,
-            initial_values: Sequence[Union[int, float]]
-            ) -> None:
+        self,
+        learning_rate: float,
+        inverse_temperature: float,
+        initial_values: Sequence[Union[int, float]],
+    ) -> None:
         self.estimated_values = np.array(initial_values, dtype=float)
 
         if (0 < learning_rate) and (learning_rate < 1.0):
             self.learning_rate = learning_rate
         else:
-            raise ValueError(f"learning_rate should be (0, 1), \
-                             but the given learning_rate is {learning_rate}")
+            raise ValueError(
+                f"learning_rate should be (0, 1), \
+                             but the given learning_rate is {learning_rate}"
+            )
 
         self.inverse_temperature = inverse_temperature
 
+        self.estimated_learning_rate = None
+        self.estimated_beta = None
+
     def choose_action(self) -> int:
-        action_probs = self._softmax()
-        chosen_action = np.random.choice(
-                len(action_probs),
-                size=1,
-                p=action_probs
-                )
+        action_probs = self._softmax(self.estimated_values, self.inverse_temperature)
+        chosen_action = np.random.choice(len(action_probs), size=1, p=action_probs)
         return chosen_action[0]
 
     def learn(self, chosen_action: int, reward: float) -> None:
-        self.estimated_values[chosen_action] = self.estimated_values[chosen_action] + self.learning_rate * (reward - self.estimated_values[chosen_action])
+        self.estimated_values[chosen_action] = self.estimated_values[
+            chosen_action
+        ] + self.learning_rate * (reward - self.estimated_values[chosen_action])
 
     def fit(
-            self,
-            observed_actions: Sequence[int],
-            observed_rewards: Sequence[Union[int, float]]
-            ) -> Sequence[float]:
-        pass
+        self,
+        n_arms: Sequence[int],
+        observed_actions: Sequence[int],
+        observed_rewards: Sequence[Union[int, float]],
+        fixed_free_params: dict,
+        init_param,
+        const=None,
+    ) -> Sequence[float]:
 
-    def _softmax(self) -> Sequence[float]:
-        exponents = self.inverse_temperature * self.estimated_values
+        params = {}
+        params.update(fixed_free_params)
+        params["n_arms"] = n_arms
+        params["observed_actions"] = observed_actions
+        params["observed_rewards"] = observed_rewards
+
+        objective = partial(self.calculate_nll, **params)
+        def obj(x):
+            return objective(*x)
+        res = minimize(
+            obj,
+            init_param,
+            method="COBYLA",
+            options={"tol": 1e-8, "disp": False, "maxiter": 100000},
+            constraints=const,
+        )
+
+        return res.x
+
+    def calculate_nll(
+        self,
+        alpha: float,
+        beta: float,
+        n_arms: int,
+        observed_actions: Sequence[int],
+        observed_rewards: Union[Sequence[int], Sequence[float]],
+    ) -> float:
+        n_trials = len(observed_actions)
+        Q = np.zeros((n_trials, n_arms), dtype=float)
+        probs = np.zeros((n_trials, n_arms), dtype=float)
+        ll = 0
+
+        for t in range(n_trials):
+            probs[t] = self._softmax(Q[t], beta)
+            ll += np.log(probs[t, observed_actions[t]])
+
+            if t < n_trials - 1:
+                Q[t + 1] += Q[t]
+                Q[t + 1, observed_actions[t]] = Q[t, observed_actions[t]] + alpha * (
+                    observed_rewards[t] - Q[t, observed_actions[t]]
+                )
+
+        return -ll
+
+    def _softmax(self, x, beta) -> Sequence[float]:
+        exponents = beta * x
         max_exp = np.max(exponents)
 
         # For numerical stability,
